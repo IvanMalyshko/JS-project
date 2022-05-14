0253af781a82ae4961db12b80bb378a87e118a46
ivan_malyshko <malyshko.ivan1@gmail.com>
2022-05-14 21:55:36 +0300

    firts commit

---
+++ alert.js
@@ -0,0 +1,8 @@
+
+// result = prompt('сколько тебе лет', 25);
+// alert(`тебе ${result} лет`);
+// podtdv = confirm('ты правильно указала свой возраст');
+// alert(podtdv)
+let userAge = prompt('how your name',);
+alert(userAge)
+prompt()
