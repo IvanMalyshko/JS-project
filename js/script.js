5f8c0ecc8b1d26f1197083c5b9666e5824b09d31
ivan_malyshko <malyshko.ivan1@gmail.com>
2022-05-15 09:56:45 +0300

    first dz

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-// // "use strict";
-// //
-// // console.log(1);
-// // let number = 5;
-// // const leftBorderWidth = 1; //camelcase
-// //
-// // number = 10
-// // console.log(number);
-// // console.log(leftBorderWidth);
+// // // "use strict";
+// // //
+// // // console.log(1);
+// // // let number = 5;
+// // // const leftBorderWidth = 1; //camelcase
+// // //
+// // // number = 10
+// // // console.log(number);
+// // // console.log(leftBorderWidth);
+// // // const obj = {
+// // //     a:5
+// // // };
+// // // obj.a = 10;
+// // // console.log(obj);
+// // // {
+// // //     let name = 'Ivan'
+// // //     const age = 21
+// // //     console.log(age);
+// // // }
 // // const obj = {
-// //     a:5
-// // };
-// // obj.a = 10;
-// // console.log(obj);
-// // {
-// //     let name = 'Ivan'
-// //     const age = 21
-// //     console.log(age);
+// //     name: 'Ivan',
+// //     age:31,
+// //     isMarried: true
 // // }
-// const obj = {
-//     name: 'Ivan',
-//     age:31,
-//     isMarried: true
-// }
-// console.log(obj.age) //=> 31
-// //массивы => часный случай обьекта хранящий элементы по порядку
-// let arr = ['Ivan', 'red.jpg', 21, {}, [], 5] //=> может хранить в себе любые типы данных
-// console.log(arr);
-// const name = prompt('How your name?')
-// alert(`Hello ${name}`)
-
-let incr = 10,
-    derc = 10;
-
-
-console.log(incr);
-console.log(derc);
-
-// ++incr; //префикс форма
-// --derc;
-console.log(++incr);
-console.log(--derc);
-
+// // console.log(obj.age) //=> 31
+// // //массивы => часный случай обьекта хранящий элементы по порядку
+// // let arr = ['Ivan', 'red.jpg', 21, {}, [], 5] //=> может хранить в себе любые типы данных
+// // console.log(arr);
+// // const name = prompt('How your name?')
+// // alert(`Hello ${name}`)
+//
+// let incr = 10,
+//     derc = 10;
+//
+//
+// console.log(incr);
+// console.log(derc);
+//
+// // ++incr; //префикс форма
+// // --derc;
+// console.log(++incr);
+// console.log(--derc);
+//
